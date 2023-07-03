8fc19cf62b7842bb8e5c9ad8954abbaf6aba64e2
houndhollis <houndhollis9797@gmail.com>
2023-07-03 22:19:54 +0900

    feat: 소셜 버튼 컴포넌트 추가

---
+++ app/(site)/component/AuthSocialButton.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { IconType } from 'react-icons';
+
+
+const AuthSocialButton = () => {
+  return (
+    <div>
+      Social Button
+    </div>
+  )
+}
+
+export default AuthSocialButton
